430b86c456b51789acad4b1bdf1387e8d2088348
Michael Rodrigues <run.michael97@gmail.com>
2023-03-13 11:55:51 -0300

    feat: auto_test for our project

---
+++ auto_test/exec_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make -C ../
+bash < testing_my_shell.txt > out_bash.txt 
+bash -c "../minishell < testing_my_shell.txt > out_mini.txt"
+awk '!/microtano/' out_mini.txt > novo_out_mini.txt
+awk '!/_=/' novo_out_mini.txt > new_out_mini.txt
+awk '!/_=/' out_bash.txt > new_out_bash.txt
+diff new_out_mini.txt new_out_bash.txt > diff.txt
+rm new_out_mini.txt new_out_bash.txt novo_out_mini.txt out_mini.txt out_bash.txt
